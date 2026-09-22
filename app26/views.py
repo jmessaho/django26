d92ed78581b9ab11da307ecb1ec2c4c611f3e92a
Jamal MESSAHO <j.messaho@gmail.com>
2026-09-22 19:09:45 +0000

    web2026

--- app26/views.py
+++ app26/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-
+from django.utils import timezone
 # Create your views here.
 def index(request):
-    a= {'myname': 'J. Messaho'}
+    hr1 = timezone.now()
+    a= {'myname': 'J. Messaho',
+        'now' : 'hr1'}
     return render(request, 'index.html', a)
