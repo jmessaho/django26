1a18ae5bc99385af7ecf2d0d48e04e6f5a3e8d1f
Jamal MESSAHO <j.messaho@gmail.com>
2026-09-22 10:55:19 +0000

    web26-122

--- website26/settings.py
+++ website26/settings.py
@@ -51,7 +51,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'website26.urls'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -115,7 +116,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
 
-STATIC_URL = 'static/'
+
 
 
 # Email
